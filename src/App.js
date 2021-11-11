00f835df854befa1a62422de54dbd986a95ec1e7
Vladimir Ventura Furcal <vladventura@outlook.es>
2021-11-11 16:43:46 -0500

    Removed boilerplate and this is the initial concept of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { getCards } from "./store/actions/cardsActions";
+import { connect } from "react-redux";
+import { useEffect } from 'react';
+import Playmat from "./components/Playmat";
+const App = (props) => {
 
-function App() {
+  const { getCards } = props;
+
+  useEffect(() => {
+    getCards();
+  }, [getCards]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Playmat />
     </div>
   );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    allCards: state.cards.cards
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getCards: () => dispatch(getCards())
+  };
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
