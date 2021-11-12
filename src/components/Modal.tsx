4c82cd60c1f3d4ae1c3c1f301434bf1814f98e6b
Vladimir Ventura Furcal <vladventura@outlook.es>
2021-11-11 19:27:45 -0500

    Made the modal a typescript component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,29 @@
+import { ReactComponentLike } from "prop-types";
+import { MouseEventHandler, ReactComponentElement } from "react";
+import "./Modal.css";
+
+interface ModalProps {
+  onClose: MouseEventHandler<HTMLSpanElement>,
+  show: boolean,
+  header: string,
+  children: ReactComponentLike
+};
+
+const Modal = ({ onClose, show, header = "Modal", children }: ModalProps) => {
+  const modal = (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <span className="modal-header-close" onClick={onClose}>
+            &times;
+          </span>
+          <h2 className="modal-header-text">{header}</h2>
+        </div>
+        <div className="modal-body">{children}</div>
+      </div>
+    </div>
+  );
+  return show ? modal : <></>;
+};
+
+export default Modal;
