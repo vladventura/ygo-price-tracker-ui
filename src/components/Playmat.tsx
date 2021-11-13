de8ef2cdce82958b92174f7460c2f75dd17ccad0
Vladimir Ventura Furcal <vladventura@outlook.es>
2021-11-12 19:55:34 -0500

    Made the card filtering a bit more readable

--- src/components/Playmat.tsx
+++ src/components/Playmat.tsx
@@ -5,18 +5,22 @@ import "./Playmat.css";
 
 const Playmat = (props: PlaymatProps) => {
   const { allCards, filter } = props;
-  const showableCards = filter.length ? (
-    allCards
-      .filter((card) => card.name.toLowerCase().includes(filter.toLowerCase()))
-      .map((card) => <Card card={card} key={card.code} />)
-  ) : (
-    allCards.map((card) => <Card card={card} key={card.code} />)
-  );
+  const showableCards =
+    allCards.length && filter.length
+      ? allCards
+          .filter((card) =>
+            card.name.toLowerCase().includes(filter.toLowerCase())
+          )
+          .map((card) => <Card card={card} key={card.code} />)
+      : allCards.map((card) => <Card card={card} key={card.code} />);
   return (
     <div className="cards-dealer">
       {allCards.length ? (
-        showableCards.length?
-        showableCards : <p className="playmat-message">No cards to show</p>
+        showableCards.length ? (
+          showableCards
+        ) : (
+          <p className="playmat-message">No cards to show</p>
+        )
       ) : (
         <p className="playmat-message">Loading cards</p>
       )}
